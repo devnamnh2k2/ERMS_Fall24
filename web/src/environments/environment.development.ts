2d22dc183a0dfd5f41c7b7c5edc664ccd5d15adb
Nam Huu Ngo <121742978+devnamnh2k2@users.noreply.github.com>
2024-12-07 12:11:29 +0700

    update

---
+++ web/src/environments/environment.development.ts
@@ -0,0 +1,27 @@
+export const environment = {
+  serverUrl: 'https://erms-d0frcfabbyhwc5df.southeastasia-01.azurewebsites.net/',
+  apiUrl: 'https://erms-d0frcfabbyhwc5df.southeastasia-01.azurewebsites.net/',
+  baseDomain: location.hostname,
+  apiUrlGoong: 'https://rsapi.goong.io/Place/AutoComplete',
+  apiKeyGoong: 'OROWRrTPOf7LICIyp997s9Miz5iFDLSTrMX1oQ4D',
+  googleID:
+    '340351071914-5t0m80o6qnf7a8q7ad66482blh4gc0e9.apps.googleusercontent.com',
+  tokenFake:
+    'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5MzQ0ODNlNS01OGQ2LTQwNTQtODAyOC03MzA4MTgxM2Q3NTIiLCJlbWFpbCI6ImFiY0BnYW1pbC5jb20iLCJuYW1lIjoiQWRtaW4iLCJleHAiOjE3MjcxOTM5NDgsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMCIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMCJ9.gNUxL53X1CFGy6IFHh-5HR-9KMljOXHDhyDOaaF-i0c',
+    firebaseConfig : {
+      apiKey: "AIzaSyBjx5McLcAYIkV_l2V1KaWJBoONz1nBLbo",
+      authDomain: "erms-8b138.firebaseapp.com",
+      projectId: "erms-8b138",
+      storageBucket: "erms-8b138.firebasestorage.app",
+      messagingSenderId: "1041166104874",
+      appId: "1:1041166104874:web:dc78c6d29bb180c0570653",
+      measurementId: "G-WQC9XVJ3TH"
+    },
+    azureConfig: {
+      accountName : 'sep490g23',
+      containerName: 'g23chatstorage',
+      sasToken: 'sp=racwdli&st=2024-11-13T02:22:11Z&se=2025-01-01T10:22:11Z&sv=2022-11-02&sr=c&sig=ikE1bTC51vdNycHggg9qHccv%2Bw1xx6GqFUAo0L7P3zc%3D',
+      key1: '1y4ILg0k4AnWxsgqRbnupbYSBAqg5i8kWm0OAa1OlQtup3owJ+O112iu4F/YlVZ3TUqieY6/RS8L+ASt3kK8pw==',
+      sasService: "https://sep490g23.blob.core.windows.net/g23chatstorage?sp=racwdli&st=2024-11-13T02:22:11Z&se=2025-01-01T10:22:11Z&sv=2022-11-02&sr=c&sig=ikE1bTC51vdNycHggg9qHccv%2Bw1xx6GqFUAo0L7P3zc%3D"
+    }
+  };
